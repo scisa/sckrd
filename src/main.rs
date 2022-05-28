3bdbd71aab640368e10cf3dbaee78477f0ca68d1
scisa <scisa@web.de>
2022-05-28 16:55:28 +0200

    entropy algorithm

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,16 @@ fn main() {
     // Args::get_args();
     // let args = Args::get_args();
     // println!("{:?}", args);
-    let bytes = match input::read_bytes::read_bytes_to_vector("/home/chris/Public/output.lime") {
+
+    let bytes = match input::read_bytes::read_bytes_to_vector("/home/chris/Downloads/test_rust/test.bin") {
         Ok(b) => b,
         Err(_) => std::process::exit(4),
     };
+
+    let key_length_bit = 128;
+    let entropy_vec = calculation::entropy::search_crypto_keys(&bytes, key_length_bit);
+
+    for entropy in entropy_vec {
+        println!("{}", entropy.entropy);
+    }
 }
